406cd1afff462e2b2806efbce362dcc6d92801e7
Batug Kocak <batugkocak@gmail.com>
2023-03-11 19:38:04 +0300

    add navigation to ProductDetailsPage

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/pages/product_details_page.dart';
 import '../model/product.dart';
 
 class ProductItem extends StatelessWidget {
-  ProductItem({Key? key, required this.item}) : super(key: key);
+  ProductItem(
+      {Key? key, required this.title, required this.id, required this.imageUrl})
+      : super(key: key);
 
-  Product item;
+  final String title;
+  final String id;
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black54,
-          title: Text(
-            item.title,
-            textAlign: TextAlign.center,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(15)),
+        //forces its child to a certain shape
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            leading: Icon(
+              Icons.favorite,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              onPressed: () {},
+            ),
           ),
-          leading: const Icon(Icons.favorite),
-          trailing: IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () {},
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => ProductDetailsPage(
+                        title: title,
+                      )));
+            },
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Image.network(
-          item.imageUrl,
-          fit: BoxFit.cover,
         ),
       ),
     );
